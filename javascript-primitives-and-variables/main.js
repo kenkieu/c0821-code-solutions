0d4037470edd01041bbda5d5b27831bcffbdfacf
Ken Kieu <kenkieu93@gmail.com>
2021-08-23 12:05:41 -0700

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,20 @@
+var fullName = 'Ken Kieu';
+console.log('value of fullName: ' + fullName);
+
+var isCool = true;
+console.log('value of isCool: ' + isCool);
+
+var totalPets = 2;
+console.log('value of totalPets: ' + totalPets);
+
+console.log('type of fullname: ' + typeof (fullName));
+console.log('type of isCool: ' + typeof (isCool));
+console.log('type of totalPets: ' + typeof (totalPets));
+
+var unknown = null;
+console.log('value of unknown: ' + unknown);
+console.log('type of unknown: ' + typeof (unknown));
+
+var never;
+console.log('value of undefined: ' + never);
+console.log('type of undefined: ' + never);
