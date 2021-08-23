4bd5e43c3782cb7bb72740af394d9d66147e1ce7
Ken Kieu <kenkieu93@gmail.com>
2021-08-23 13:11:20 -0700

    Completed javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,38 @@
+var width = 20;
+var height = 10;
+var area = width * height;
+console.log('value of area: ' + area);
+console.log('type of area : ' + typeof area);
+
+var bill = 100;
+var payment = 101;
+var change = payment - bill;
+console.log('value of change: $' + change);
+console.log('type of change: ' + typeof change);
+
+var quizzes = 90;
+var midterm = 85;
+var final = 97;
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade: ' + grade);
+console.log('type of grade: ' + typeof grade);
+
+var firstName = 'Ken';
+var lastName = 'Kieu';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName: ' + fullName);
+console.log('type of fullName: ' + typeof fullName);
+
+var pH = 10;
+var isAcidic = pH < 7;
+console.log('value of isAcidic: ' + isAcidic);
+console.log('type of isAcdic: ' + typeof isAcidic);
+
+var headCount = 18;
+var isSparta = headCount === 300;
+console.log('value of isSparta: ' + isSparta);
+console.log('type of isSparta:  ' + typeof isSparta);
+
+var motto = fullName += ' is the GOAT';
+console.log('value of motto: ' + motto);
+console.log('type of motto: ' + typeof motto);
