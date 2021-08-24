4da04c55d00bb22753131ec351d0eb64e79a2e85
Ken Kieu <kenkieu93@gmail.com>
2021-08-24 11:49:49 -0700

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,106 @@
+var orderHistory = [
+  [
+    {
+      orderPlaced: 'Aug 04, 2020',
+      orderNumber: '114-3941689-8772232',
+      shipTo: 'JS Masher'
+    },
+
+    {
+      itemName: ['JavaScript for impatient programmers'],
+      author: ['Rauschmayer, Dr.Axel']
+    },
+
+    {
+      delivered: true,
+      deliveryEstimate: 'Aug 08, 2020',
+      deliveryHandOff: false,
+      returnWindow: ['Sep 7, 2020'],
+      productSupport: false
+
+    },
+
+    {
+      itemCost: [31.55],
+      cartTotal: 34
+    }
+  ],
+
+  [
+    {
+      orderPlaced: 'Jul 19, 2020',
+      orderNumber: '113-998-4268-1280257',
+      shipTo: 'JS Masher'
+    },
+
+    {
+      itemName: ['The Timeless Way of Building'],
+      author: ['Alexander Christopher']
+    },
+
+    {
+      delivered: true,
+      deliveryEstimate: 'Jul 20, 2020',
+      deliveryHandOff: true,
+      returnWindow: ['Aug 19, 2020'],
+      productSupport: false
+    },
+
+    {
+      itemCost: [41.33],
+      cartTotal: 44.53
+    }
+  ],
+
+  [
+    {
+      orderPlaced: 'Jul 07, 2020',
+      orderNumber: '114-2875557-9059409',
+      shipTo: 'JS Masher'
+    },
+
+    {
+      itemName: ['Gamecube Controller Adapter'],
+      author: [null]
+    },
+
+    {
+      delivered: true,
+      deliveryEstimate: 'Jul 07, 2020',
+      deliveryHandOff: true,
+      returnWindow: ['Aug 05, 2020'],
+      productSupport: false
+    },
+
+    {
+      itemCost: [15.98],
+      cartTotal: 17.22
+    }
+  ],
+
+  [
+    {
+      orderPlaced: 'Jul 03, 2020',
+      orderNumber: '113-2883177-2648248',
+      shipTo: 'JS Masher'
+    },
+
+    {
+      itemName: ['GameCube Controller', 'The Art of Sql'],
+      author: [null, 'Faroult, Stephanie']
+    },
+
+    {
+      delivered: true,
+      deliveryEstimate: 'Jul 05, 2020',
+      deliveryHandOff: false,
+      returnWindow: ['Aug 04, 2020'],
+      productSupport: true
+    },
+
+    {
+      itemCost: [94.98, 33.99],
+      cartTotal: 138.93
+    }
+  ]
+];
