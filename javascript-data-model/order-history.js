e8e2b77526d1bc90effba99f4f88db769a7aa26e
Ken Kieu <kenkieu93@gmail.com>
2021-08-24 12:36:40 -0700

    Completed javascript-data-model exercise.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,17 +1,17 @@
 var orderHistory = [
-  [
-    {
+  {
+    orderDetails: {
       orderPlaced: 'Aug 04, 2020',
       orderNumber: '114-3941689-8772232',
-      shipTo: 'JS Masher'
+      recipient: 'JS Masher'
     },
 
-    {
+    itemDetails: {
       itemName: ['JavaScript for impatient programmers'],
       author: ['Rauschmayer, Dr.Axel']
     },
 
-    {
+    deliveryDetails: {
       delivered: true,
       deliveryEstimate: 'Aug 08, 2020',
       deliveryHandOff: false,
@@ -20,25 +20,25 @@ var orderHistory = [
 
     },
 
-    {
+    costDetails: {
       itemCost: [31.55],
       cartTotal: 34
     }
-  ],
+  },
 
-  [
-    {
+  {
+    orderDetails: {
       orderPlaced: 'Jul 19, 2020',
       orderNumber: '113-998-4268-1280257',
-      shipTo: 'JS Masher'
+      recipient: 'JS Masher'
     },
 
-    {
+    itemDetails: {
       itemName: ['The Timeless Way of Building'],
       author: ['Alexander Christopher']
     },
 
-    {
+    deliveryDetails: {
       delivered: true,
       deliveryEstimate: 'Jul 20, 2020',
       deliveryHandOff: true,
@@ -46,25 +46,25 @@ var orderHistory = [
       productSupport: false
     },
 
-    {
+    costDetails: {
       itemCost: [41.33],
       cartTotal: 44.53
     }
-  ],
+  },
 
-  [
-    {
+  {
+    orderDetails: {
       orderPlaced: 'Jul 07, 2020',
       orderNumber: '114-2875557-9059409',
-      shipTo: 'JS Masher'
+      recipient: 'JS Masher'
     },
 
-    {
+    itemDetails: {
       itemName: ['Gamecube Controller Adapter'],
       author: [null]
     },
 
-    {
+    deliveryDetails: {
       delivered: true,
       deliveryEstimate: 'Jul 07, 2020',
       deliveryHandOff: true,
@@ -72,25 +72,25 @@ var orderHistory = [
       productSupport: false
     },
 
-    {
+    costDetails: {
       itemCost: [15.98],
       cartTotal: 17.22
     }
-  ],
+  },
 
-  [
-    {
+  {
+    orderDetails: {
       orderPlaced: 'Jul 03, 2020',
       orderNumber: '113-2883177-2648248',
-      shipTo: 'JS Masher'
+      recipient: 'JS Masher'
     },
 
-    {
+    itemDetails: {
       itemName: ['GameCube Controller', 'The Art of Sql'],
       author: [null, 'Faroult, Stephanie']
     },
 
-    {
+    deliveryDetails: {
       delivered: true,
       deliveryEstimate: 'Jul 05, 2020',
       deliveryHandOff: false,
@@ -98,9 +98,9 @@ var orderHistory = [
       productSupport: true
     },
 
-    {
+    costDetails: {
       itemCost: [94.98, 33.99],
       cartTotal: 138.93
     }
-  ]
+  }
 ];
