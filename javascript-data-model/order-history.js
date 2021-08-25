6cd5071be6b0f5ea3aea764936339bb02ac7c48c
Ken Kieu <kenkieu93@gmail.com>
2021-08-24 18:23:22 -0700

    Revision for javascript-data-model exercise.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -3,95 +3,74 @@ var orderHistory = [
     orderPlaced: 'Aug 04, 2020',
     orderNumber: '114-3941689-8772232',
     recipient: 'JS Masher',
-    author: ['Rauschmayer, Dr.Axel'],
     delivered: true,
     deliveryEstimate: 'Aug 08, 2020',
     deliveryHandOff: false,
     returnWindow: 'Sep 7, 2020',
-    productSupport: false
-    items: ['JavaScript for impatient programmers'],
-    },
-
-    costDetails: {
-      itemCost: [31.55],
-      cartTotal: 34
+    cartTotal: 34.00,
+    items:
+    [{
+      type: 'Book',
+      author: 'Rauschmayer, Dr.Axel',
+      name: 'JavaScript for impatient programmers',
+      cost: 31.55
+    }]
   },
-
   {
-    orderDetails: {
-      orderPlaced: 'Jul 19, 2020',
-      orderNumber: '113-998-4268-1280257',
-      recipient: 'JS Masher'
-    },
-
-    itemDetails: {
-      itemName: ['The Timeless Way of Building'],
-      author: ['Alexander Christopher']
-    },
-
-    deliveryDetails: {
-      delivered: true,
-      deliveryEstimate: 'Jul 20, 2020',
-      deliveryHandOff: true,
-      returnWindow: ['Aug 19, 2020'],
-      productSupport: false
-    },
-
-    costDetails: {
-      itemCost: [41.33],
-      cartTotal: 44.53
-    }
+    orderPlaced: 'Jul 19, 2020',
+    orderNumber: '113-998-4268-1280257',
+    recipient: 'JS Masher',
+    delivered: true,
+    deliveryEstimate: 'Jul 20, 2020',
+    deliveryHandOff: true,
+    returnWindow: 'Aug 19, 2020',
+    cartTotal: 44.53,
+    items:
+    [{
+      type: 'Book',
+      author: 'Alexander Christopher',
+      name: 'The Timeless Way of Building',
+      cost: 41.33
+    }]
   },
-
   {
-    orderDetails: {
-      orderPlaced: 'Jul 07, 2020',
-      orderNumber: '114-2875557-9059409',
-      recipient: 'JS Masher'
-    },
-
-    itemDetails: {
-      itemName: ['Gamecube Controller Adapter'],
-      author: [null]
-    },
-
-    deliveryDetails: {
-      delivered: true,
-      deliveryEstimate: 'Jul 07, 2020',
-      deliveryHandOff: true,
-      returnWindow: ['Aug 05, 2020'],
-      productSupport: false
-    },
-
-    costDetails: {
-      itemCost: [15.98],
-      cartTotal: 17.22
-    }
+    orderPlaced: 'Jul 04, 2020',
+    orderNumber: '114-2875557-9059409',
+    recipient: 'JS Masher',
+    delivered: true,
+    deliveryEstimate: 'Jul 07, 2020',
+    deliveryHandOff: true,
+    returnWindow: 'Aug 05, 2020',
+    cartTotal: 17.22,
+    items:
+    [{
+      type: 'Electronics',
+      productSupport: false,
+      name: 'Gamecube Controller Adapter',
+      cost: 15.98
+    }]
   },
-
   {
-    orderDetails: {
-      orderPlaced: 'Jul 03, 2020',
-      orderNumber: '113-2883177-2648248',
-      recipient: 'JS Masher'
+    orderPlaced: 'Jul 03, 2020',
+    orderNumber: '113-2883177-2648248',
+    recipient: 'JS Masher',
+    delivered: true,
+    deliveryEstimate: 'Jul 05, 2020',
+    deliveryHandOff: false,
+    returnWindow: 'Aug 04, 2020',
+    cartTotal: 138.93,
+    items:
+    [{
+      type: 'Electronics',
+      productSupport: true,
+      name: 'GameCube Controller',
+      cost: 94.95
     },
-
-    itemDetails: {
-      itemName: ['GameCube Controller', 'The Art of Sql'],
-      author: [null, 'Faroult, Stephanie']
-    },
-
-    deliveryDetails: {
-      delivered: true,
-      deliveryEstimate: 'Jul 05, 2020',
-      deliveryHandOff: false,
-      returnWindow: ['Aug 04, 2020'],
-      productSupport: true
-    },
-
-    costDetails: {
-      itemCost: [94.98, 33.99],
-      cartTotal: 138.93
-    }
+    {
+      type: 'Book',
+      author: 'Faroult, Stephanie',
+      name: 'The Art of Sql',
+      cost: 33.99
+    }]
   }
 ];
