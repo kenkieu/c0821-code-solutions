6c6d4cc7fbbd7ee7c1af998a8658dd527e120da3
Ken Kieu <kenkieu93@gmail.com>
2021-08-27 21:37:11 -0700

    Revisions for string-manipulation-1 exercise.

--- string-manipulation-1/src/get-first-char.js
+++ string-manipulation-1/src/get-first-char.js
@@ -3,22 +3,8 @@
 // input: string
 // output: first character of string
 
-// Create storage for output
-// Check each char to determine if it is the first character
-// if first character or only character
-// add current letter to output
-// return the final output
+// return first letter
 
 function getFirstChar(string) {
-  var firstChar = '';
-  for (var i = 0; i < string.length; i++) {
-    if (string[i] === string[0]) {
-      firstChar = string[i];
-    }
-  }
-  return firstChar;
+  return string[0];
 }
-
-// function getFirstChar(string) {
-//   return string[0];
-// }
