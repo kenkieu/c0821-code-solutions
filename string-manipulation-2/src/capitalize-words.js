951a3937de8b798c0d055366dccff77f3b38562c
Ken Kieu <kenkieu93@gmail.com>
2021-08-28 22:45:43 -0700

    In Progress

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,3 +1,27 @@
 /* exported capitalizeWords */
-// input:
-// output:
+// input: string
+// output: string all words capitalized
+// create storage for new word
+// look at each character
+// if space then word
+// first letter of each word capitalized
+// store into output
+// if end of input, then word
+// capitalize first letter
+// store into output
+
+function capitalizeWords (string){
+  var newString = '';
+  var currentWord = string[0].toUpperCase();
+
+  for(var i = 1; i < string.length; i++){
+    if(string[i] === " "){
+      newString += currentWord + " "
+      currentWord = '';
+    } else {
+      currentWord += string[i].toLowerCase();
+    }
+  }
+  newString += currentWord;
+  return newString;
+}
