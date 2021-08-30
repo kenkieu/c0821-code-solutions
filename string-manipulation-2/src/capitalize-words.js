b2fdc4b94f41c3673dbe6c0ada13e12a62b99606
Ken Kieu <kenkieu93@gmail.com>
2021-08-29 20:22:58 -0700

    Completed string-manipulation-2 exercise.

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -2,26 +2,27 @@
 // input: string
 // output: string all words capitalized
 // create storage for new word
+// create storage for current word being modified
 // look at each character
 // if space then word
-// first letter of each word capitalized
-// store into output
-// if end of input, then word
-// capitalize first letter
-// store into output
+// take first character, uppercase it, attach remaining letters and store into output
+// store into output, and add space for following word
+// clear current word
+// otherwise, adding remaining characters to current word storage if there are no spaces and making all lowercase
+// last word in input string, take first character, uppercase it, attach remaining letters and store into output
+// return word storage
 
 function capitalizeWords (string){
   var newString = '';
-  var currentWord = string[0].toUpperCase();
-
-  for(var i = 1; i < string.length; i++){
+  var currentWord = '';
+  for(var i = 0; i < string.length; i++){
     if(string[i] === " "){
-      newString += currentWord + " "
+      newString += currentWord.slice(0, 1).toUpperCase() + currentWord.slice(1) + " ";
       currentWord = '';
     } else {
       currentWord += string[i].toLowerCase();
     }
   }
-  newString += currentWord;
+  newString += currentWord.slice(0, 1).toUpperCase() + currentWord.slice(1);
   return newString;
 }
--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,3 +1,26 @@
 /* exported swapChars */
-// input:
-// output:
+// input 1: number
+// input 2: number
+// input 3: string
+// output: string with swapped first and second index
+// create word storage
+// look at each character
+// if the char is firstIndex, switch with secondIndex
+// also if char is secondIndex switch with firstIndex
+// otherwise continue to add characters to word storage
+// return word storage
+
+function swapChars(firstIndex, secondIndex, string){
+  var modStr = '';
+  for(var i = 0; i < string.length; i++) {
+    if(i === firstIndex) {
+      modStr += string[secondIndex]
+    }
+    else if (i === secondIndex) {
+      modStr += string[firstIndex]
+    } else{
+      modStr += string[i]
+    }
+  }
+  return modStr;
+}
