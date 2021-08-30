c825832172d7bcacbe3bbf2b31c911d9cf1aca8c
Ken Kieu <kenkieu93@gmail.com>
2021-08-29 20:22:58 -0700

    Completed string-manipulation-2 exercise.

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,3 +1,22 @@
 /* exported lastChars */
-// input:
-// output:
+// input: Number
+// output: string
+// create storage for new word
+// look at each character starting at position of the length of the input word
+// subtracted by the length (number) of the input
+// if the length input exceeds the length of the word
+// word storage equals the word
+// otherwise, starting from the specified number add each letter into the word storage
+// return the word storage
+
+function lastChars (length, string) {
+  var modStr = ''
+  for(var i = string.length - length; i < string.length; i++) {
+    if(length > string.length) {
+      modStr = string;
+    } else{
+      modStr += string[i]
+    }
+  }
+  return modStr;
+}
--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,3 +1,23 @@
 /* exported numVowels */
-// input:
-// output:
+// input: string
+// output: number of vowel characters in string
+// create reference data for all possible vowels
+// create storage to keep track of vowel count
+// look at each character letter
+// reference each letter and look at possible vowels
+// if vowel or capital vowel
+// add one to counter storage
+// return the number counter
+
+function numVowels(string){
+  var vowelsArr = ["a", "e", "i", "o", "u"];
+  var counter = 0;
+  for(var i = 0; i < string.length; i++) {
+    for(var x = 0; x < vowelsArr.length; x++){
+      if(string[i] === vowelsArr[x] || string[i] === vowelsArr[x].toUpperCase()){
+        counter++
+      }
+    }
+  }
+  return counter;
+}
