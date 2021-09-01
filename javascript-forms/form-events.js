d721414787af9c7aaae2f59c6abf1db655e8b471
Ken Kieu <kenkieu93@gmail.com>
2021-09-01 12:28:41 -0700

    Splitting code into respective js files.

---
+++ javascript-forms/form-events.js
@@ -0,0 +1,31 @@
+function handleFocus(event) {
+  console.log('Focus event has fired');
+  console.log('value of event.target.name:', event.target.name);
+}
+
+function handleBlur(event) {
+  console.log('Blur event has fired');
+  console.dir(event.target);
+  console.log('value of event.target.name:', event.target.name);
+}
+
+function handleInput(event) {
+  console.log('value of event.target.name:', event.target.name);
+  console.log('value of event.target.value:', event.target.value);
+}
+
+var $inputName = document.querySelector('input#user-name');
+var $inputEmail = document.querySelector('input#user-email');
+var $textarea = document.querySelector('textarea');
+
+$inputName.addEventListener('focus', handleFocus);
+$inputName.addEventListener('blur', handleBlur);
+$inputName.addEventListener('input', handleInput);
+
+$inputEmail.addEventListener('focus', handleFocus);
+$inputEmail.addEventListener('blur', handleBlur);
+$inputEmail.addEventListener('input', handleInput);
+
+$textarea.addEventListener('focus', handleFocus);
+$textarea.addEventListener('blur', handleBlur);
+$textarea.addEventListener('input', handleInput);
