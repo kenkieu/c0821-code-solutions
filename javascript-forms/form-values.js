4834d69a4ba249ce1183d00212d32615086c484f
Ken Kieu <kenkieu93@gmail.com>
2021-09-01 16:43:54 -0700

    Revision for javascript-forms exercise.

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -1,7 +1,7 @@
-var messageData = {};
 
 function handleContactForm(event) {
   event.preventDefault();
+  var messageData = {};
   messageData.name = $contactForm.elements.name.value;
   messageData.email = $contactForm.elements.email.value;
   messageData.message = $contactForm.elements.message.value;
