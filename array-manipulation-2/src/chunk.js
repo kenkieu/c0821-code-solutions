0b65dd481b73778fc159c0a72eb1aff1a778b33d
Ken Kieu <kenkieu93@gmail.com>
2021-08-31 22:15:26 -0700

    In Progress

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,3 +1,21 @@
 /* exported chunk */
-//input:
-//output:
+//input 1: Array
+//input 2: number determining chunk size
+//output: array of arrays
+//create array to store arrays
+//
+
+function chunk(array, size){
+  var arrB = [];
+  for(var i = 0; i < array.length; i++){
+    var arr = [];
+    for(var x = 0; x < array.length; x++){
+      arr.push(array[x])
+    }
+    console.log(arr)
+    return arr;
+  }
+var masterArr = arrB.concat(arr);
+console.log(masterArr)
+return masterArr;
+}
--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,3 +1,23 @@
 /* exported dropRight */
-// input:
-// output:
+// input 1: array
+// input 2: number
+// output: new array containing the elements of array before the last count elements
+// create storage for new array
+// if nothing in array
+// return empty array
+// look at each item of array
+// move items to array for the modified length
+// return new array
+// length - 2
+
+function dropRight(array, number){
+  var arr = [];
+  if(!array.length) {
+    return arr;
+  }
+  for(var i = 0; i < array.length - number; i++) {
+    arr.push(array[i])
+  }
+  console.log(arr)
+  return arr;
+}
