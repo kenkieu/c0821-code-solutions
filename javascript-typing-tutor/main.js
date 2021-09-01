8f9fc7a48a375c8e1660a53f16181f0819bcfc29
Ken Kieu <kenkieu93@gmail.com>
2021-08-31 20:55:18 -0700

    Completed javascript-typing-tutor challenge.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,16 +1,23 @@
+//create storage to keep track of count
+//query for all span elements & store
+//listen for keyboard down presses, and when this occurs call the function handleKeys
+//if the key press is the same as the text character make it purple (correct)
+//add 1 to counter
+//add the underline style to the next word (this is already set on the first char)
+//also if the wrong key is entered, make the text orange (wrong)
+
+var counter = 0;
+
 function handleKeys(event) {
-
-  var counter = 0;
-  var $spanEl = document.querySelectorAll('span');
-  if ($spanEl[counter].textContent === event.key) {
-    console.log(event.key);
-    $spanEl[counter].className = 'underline';
-    $spanEl[counter].className = 'text-correct';
-    counter++;
+  if (event.key === $nodeList[counter].textContent) {
+    $nodeList[counter].className = "text-correct"
+    counter++
+    $nodeList[counter].className = "underline"
   }
-
-  console.log($spanEl);
-  document.addEventListener('keydown', handleKeys);
+  else if (event.key !== $nodeList[counter].textContent){
+    $nodeList[counter].className = "text-error"
+  }
 }
 
-// NODELIST
+var $nodeList = document.querySelectorAll('span');
+document.addEventListener("keydown", handleKeys)
