9169b5c3b656ab619cdfa1fc40fe35564c23d7c9
Ken Kieu <kenkieu93@gmail.com>
2021-09-02 11:50:13 -0700

    Revision for dom-event-delegation exercise.

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -1,7 +1,7 @@
 function handleClick(event) {
   console.log('value of event.target :', event.target);
   console.log('value of event.target.tagName:', event.target.tagName);
-  if (event.target.matches('button') && event.target.tagName === 'BUTTON') {
+  if (event.target.matches('button')) {
     var closestEl = event.target.closest('.task-list-item');
     console.log('value of closest .task-list-item:', closestEl);
     closestEl.remove();
