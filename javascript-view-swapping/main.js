343f5eb2696c7610fb7619c83083f3412c7f69f5
Ken Kieu <kenkieu93@gmail.com>
2021-09-02 14:54:40 -0700

    Completed javascript-view-swapping exercise.

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -2,21 +2,25 @@ function handleClick(event) {
   if (event.target.matches('.tab')) {
     for (var i = 0; i < $tab.length; i++) {
       if ($tab[i] === event.target) {
-        event.target.className = 'tab active';
+        $tab[i].className = 'tab active';
+        var attr = $tab[i].getAttribute('data-view');
       } else {
-        event.target.className = 'tab';
+        $tab[i].className = 'tab';
+      }
+    }
+
+    for (var x = 0; x < $view.length; x++) {
+      if ($view[x].dataset.view === attr) {
+        $view[x].className = 'view';
+      } else {
+        $view[x].className = 'view hidden';
       }
     }
   }
-
 }
 
 var $tabContainer = document.querySelector('.tab-container');
 var $tab = document.querySelectorAll('.tab');
 var $view = document.querySelectorAll('.view');
-
-console.log('$tabContainer:', $tabContainer);
-console.log('$tab', $tab);
-console.log('$view', $view);
 
 $tabContainer.addEventListener('click', handleClick);
