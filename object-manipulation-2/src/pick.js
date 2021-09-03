4f265c0fcf01a69baaee30845bc4536093321725
Ken Kieu <kenkieu93@gmail.com>
2021-09-02 18:00:41 -0700

    In Progress

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -3,8 +3,12 @@
 // input 2: strings
 // output: A new Object containing all of the properties of source listed in keys If key is
 // create storage for new obj
-// obtain each key in obj
-//
+// look at each item in keys
+// look at items in source input
+// if the source input value is the same as the keys input at the specified i
+// set the property and value of the source input to new obj
+// return new obj
+
 function pick(source, keys) {
   var obj = {};
   for (var i = 0; i < keys.length; i++) {
