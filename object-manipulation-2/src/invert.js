7630326162a1520797eec9ed4f4afd703f1955e5
Ken Kieu <kenkieu93@gmail.com>
2021-09-04 00:27:50 -0700

    In Progress

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,3 +1,17 @@
 /* exported invert */
-// input:
-// output:
+// input: object
+// output: new object with inverted values
+// create storage for new object
+// look at each of the keys in source
+// set storage for values of source
+// assign the key to the property to inverse
+// return new object
+
+function invert(source) {
+  var outputObj = {};
+  for (var key in source) {
+    var value = source[key];
+    outputObj[value] = key;
+  }
+  return outputObj;
+}
--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,21 +1,29 @@
+
 /* exported omit */
 // input: Object
 // input: Array of strings
 // output: new object with all properties that aren't listed in keys
 // create storage for new obj
-// look over each item in keys array input
-// if there is no value in source for that property
-// assign value to outputObj property
-// return outputObj
+// look over each key in source input
+// store a boolean set to false by default
+// look at each item in keys input
+// if the current key is equal to the key of the source
+// set key exists to true
+// if key exists is false
+// add the property and assign it's value to the new obj
+// return the new obj
 
 function omit(source, keys) {
   var outputObj = {};
-  for (var i = 0; i < keys.length; i++) {
-    console.log(source[keys[i]]);
-    console.log(keys[i]);
-    console.log(source[keys]);
-    if (!source[keys[i]]) {
-      outputObj[keys[i]] = source[keys];
+  for (var key in source) {
+    var keyExists = false;
+    for (var i = 0; i < keys.length; i++) {
+      if (keys[i] === key) {
+        keyExists = true;
+      }
+    }
+    if (keyExists === false) {
+      outputObj[key] = source[key];
     }
   }
   return outputObj;
