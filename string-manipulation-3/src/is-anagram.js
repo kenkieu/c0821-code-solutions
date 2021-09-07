b61f0986c5180e93ea0f101d2e5c554fa26912f7
Ken Kieu <kenkieu93@gmail.com>
2021-09-06 17:02:53 -0700

    In Progress

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,1 +1,34 @@
 /* exported isAnagram */
+// input 1: string
+// input 2: string
+// output: boolean
+// create storage for firstArr
+// create storage for secondArr
+// look over and add each character into firstArr for firstString
+// look over second string
+// if not a space
+// add the character to the secondArr
+// sort first and second arr
+// if firstArr is equal to secondArr then return true
+
+function isAnagram(firstString, secondString) {
+  var firstArr = [];
+  var secondArr = [];
+  for (var i = 0; i < firstString.length; i++) {
+    if (firstString[i] !== ' ') {
+      firstArr.push(firstString[i]);
+    }
+  }
+  for (var i = 0; i < secondString.length; i++) {
+    if (secondString[i] !== ' ') {
+      secondArr.push(secondString[i]);
+    }
+  }
+  var sortedFirstWord = firstArr.sort().join('');
+  var sortedSecondWord = secondArr.sort().join('');
+  if (sortedFirstWord === sortedSecondWord) {
+    return true;
+  } else {
+    return false;
+  }
+}
--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,32 @@
 /* exported reverseWords */
+// input: string
+// output: string
+
+function reverseWords(string) {
+  var word = '';
+  var reverseWord = '';
+  var strArr = [];
+  var reverseArr = [];
+  for (var i = 0; i < string.length; i++) {
+    if (string[i] !== ' ') {
+      word += string[i];
+    } else {
+      strArr.push(word);
+      word = '';
+    }
+  }
+  strArr.push(word);
+  console.log(strArr);
+
+  // for (var x = 0; x < strArr.length; x++) {
+  //   // console.log(strArr[x]);
+  //   for (var y = strArr[x].length - 1; y >= 0; y--) {
+  //     // console.log(strArr[x][y]);
+  //     reverseArr.push(strArr[x][y]);
+  //     console.log(reverseArr);
+  //     reverseWord = reverseArr.join(' ');
+  //   }
+  // }
+  // console.log(reverseWord);
+  // return reverseWord;
+}
