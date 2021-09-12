d28fe9b0687313ef31fa06e28d13d0ecbfd29cc1
Ken Kieu <kenkieu93@gmail.com>
2021-09-12 16:59:57 -0700

    In Progress.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -10,18 +10,87 @@
 
 // function titleCase(string) {
 //   var newString = '';
-//   var currentWord = '';
-//   for (var i = 0; i < string.length; i++) {
-//     if (string[i] === ' ') {
-//       newString += currentWord[0].toUpperCase();
-//       newString += currentWord.slice(1) + ' ';
-//       currentWord = '';
+//   var splitString = string.split(' ');
+//   var firstWord = splitString[0];
+//   var lastWord = splitString[splitString.length - 1];
+//   if (firstWord !== ('javascript:' || 'Javascript:')) {
+//     newString += firstWord[0].toUpperCase();
+//     newString += firstWord.slice(1) + ' ';
+//   } else {
+//     newString = 'JavaScript:' + ' ';
+//   }
+//   for (var i = 1; i < splitString.length; i++) {
+//     // console.log(splitString[i]);
+//     if (splitString[i] === 'api') {
+//       newString += 'API';
+//     } else if (splitString[i] === ('Javascript:')) {
+//       newString += 'JavaScript:' + ' ';
+//     } else if (splitString[i] === ('javascript' || 'Javascript')) {
+//       newString += 'JavaScript' + ' ';
+//     } else if (splitString[i].length < 4 && !'web') {
+//       newString += splitString[i].toLowerCase() + ' ';
+//     } else if (splitString[i].includes('-')) {
+//       var hyphenWord = splitString[i].split('-');
+//       for (var x = 0; x < hyphenWord.length; x++) {
+//         if (hyphenWord[x] !== hyphenWord[hyphenWord.length - 1]) {
+//           newString += hyphenWord[x][0].toUpperCase();
+//           newString += hyphenWord[x].slice(1) + '-';
+//         } else {
+//           newString += hyphenWord[x][0].toUpperCase();
+//           newString += hyphenWord[x].slice(1) + ' ';
+//         }
+//       }
+//     } else if (splitString[i] !== lastWord) {
+//       newString += splitString[i][0].toUpperCase();
+//       newString += splitString[i].slice(1) + ' ';
 //     } else {
-//       currentWord += string[i].toLowerCase();
+//       newString += splitString[i][0].toUpperCase();
+//       newString += splitString[i].slice(1);
 //     }
 //   }
-//   newString += currentWord[0].toUpperCase();
-//   newString += currentWord.slice(1);
 //   console.log(newString);
 //   return newString;
 // }
+
+function titleCase(string) {
+  var newString = '';
+  var splitString = string.split(' ');
+  var firstWord = splitString[0];
+  var lastWord = splitString[splitString.length - 1];
+  if (firstWord !== ('javascript:' || 'Javascript:')) {
+    newString += firstWord[0].toUpperCase();
+    newString += firstWord.slice(1) + ' ';
+  } else {
+    newString = 'JavaScript:' + ' ';
+  }
+  for (var i = 1; i < splitString.length; i++) {
+    // console.log(splitString[i]);
+    if (splitString[i] === 'api') {
+      newString += 'API';
+    } else if (splitString[i] === ('Javascript:')) {
+      newString += 'JavaScript:' + ' ';
+    } else if (splitString[i] === ('javascript' || 'Javascript')) {
+      newString += 'JavaScript' + ' ';
+    } else if (splitString[i].length < 4 && splitString[i] !== 'web') {
+      newString += splitString[i].toLowerCase() + ' ';
+    } else if (splitString[i].includes('-')) {
+      var hyphenWord = splitString[i].split('-');
+      for (var x = 0; x < hyphenWord.length; x++) {
+        if (hyphenWord[x] !== hyphenWord[hyphenWord.length - 1]) {
+          newString += hyphenWord[x][0].toUpperCase();
+          newString += hyphenWord[x].slice(1) + '-';
+        } else {
+          newString += hyphenWord[x][0].toUpperCase();
+          newString += hyphenWord[x].slice(1) + ' ';
+        }
+      }
+    } else if (splitString[i] !== lastWord) {
+      newString += splitString[i][0].toUpperCase();
+      newString += splitString[i].slice(1) + ' ';
+    } else {
+      newString += splitString[i][0].toUpperCase();
+      newString += splitString[i].slice(1);
+    }
+  }
+  return newString;
+}
