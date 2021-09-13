5f0c61fc62c78d8366de43aa91cc20e0dc0d9641
Ken Kieu <kenkieu93@gmail.com>
2021-09-12 20:33:56 -0700

    In Progress

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -8,50 +8,6 @@
 //* if word is less than 4 characters and not at first position character then make lowercase
 //* if word at current position is javascript or Javascript, then word is JavaScript
 
-// function titleCase(string) {
-//   var newString = '';
-//   var splitString = string.split(' ');
-//   var firstWord = splitString[0];
-//   var lastWord = splitString[splitString.length - 1];
-//   if (firstWord !== ('javascript:' || 'Javascript:')) {
-//     newString += firstWord[0].toUpperCase();
-//     newString += firstWord.slice(1) + ' ';
-//   } else {
-//     newString = 'JavaScript:' + ' ';
-//   }
-//   for (var i = 1; i < splitString.length; i++) {
-//     // console.log(splitString[i]);
-//     if (splitString[i] === 'api') {
-//       newString += 'API';
-//     } else if (splitString[i] === ('Javascript:')) {
-//       newString += 'JavaScript:' + ' ';
-//     } else if (splitString[i] === ('javascript' || 'Javascript')) {
-//       newString += 'JavaScript' + ' ';
-//     } else if (splitString[i].length < 4 && !'web') {
-//       newString += splitString[i].toLowerCase() + ' ';
-//     } else if (splitString[i].includes('-')) {
-//       var hyphenWord = splitString[i].split('-');
-//       for (var x = 0; x < hyphenWord.length; x++) {
-//         if (hyphenWord[x] !== hyphenWord[hyphenWord.length - 1]) {
-//           newString += hyphenWord[x][0].toUpperCase();
-//           newString += hyphenWord[x].slice(1) + '-';
-//         } else {
-//           newString += hyphenWord[x][0].toUpperCase();
-//           newString += hyphenWord[x].slice(1) + ' ';
-//         }
-//       }
-//     } else if (splitString[i] !== lastWord) {
-//       newString += splitString[i][0].toUpperCase();
-//       newString += splitString[i].slice(1) + ' ';
-//     } else {
-//       newString += splitString[i][0].toUpperCase();
-//       newString += splitString[i].slice(1);
-//     }
-//   }
-//   console.log(newString);
-//   return newString;
-// }
-
 function titleCase(string) {
   var newString = '';
   var splitString = string.split(' ');
@@ -64,17 +20,20 @@ function titleCase(string) {
     newString = 'JavaScript:' + ' ';
   }
   for (var i = 1; i < splitString.length; i++) {
-    // console.log(splitString[i]);
-    if (splitString[i] === 'api') {
+    var currentWord = splitString[i].toLowerCase();
+    var previousWord = splitString[i - 1];
+    if (currentWord === 'api') {
       newString += 'API';
-    } else if (splitString[i] === ('Javascript:')) {
-      newString += 'JavaScript:' + ' ';
-    } else if (splitString[i] === ('javascript' || 'Javascript')) {
-      newString += 'JavaScript' + ' ';
-    } else if (splitString[i].length < 4 && splitString[i] !== 'web') {
-      newString += splitString[i].toLowerCase() + ' ';
-    } else if (splitString[i].includes('-')) {
-      var hyphenWord = splitString[i].split('-');
+    } else if (currentWord.includes("javascript")) {
+      if (currentWord.includes(":")) {
+        newString += "JavaScript:" + ' '
+      } else {
+        newString += 'JavaScript' + ' ';
+      }
+    } else if (currentWord.length < 4 && currentWord !== 'web' && !previousWord.includes(":")) {
+      newString += currentWord.toLowerCase() + ' ';
+    } else if (currentWord.includes('-')) {
+      var hyphenWord = currentWord.split('-');
       for (var x = 0; x < hyphenWord.length; x++) {
         if (hyphenWord[x] !== hyphenWord[hyphenWord.length - 1]) {
           newString += hyphenWord[x][0].toUpperCase();
@@ -84,13 +43,14 @@ function titleCase(string) {
           newString += hyphenWord[x].slice(1) + ' ';
         }
       }
-    } else if (splitString[i] !== lastWord) {
-      newString += splitString[i][0].toUpperCase();
-      newString += splitString[i].slice(1) + ' ';
+    } else if (currentWord !== lastWord) {
+      newString += currentWord[0].toUpperCase();
+      newString += currentWord.slice(1) + ' ';
     } else {
-      newString += splitString[i][0].toUpperCase();
-      newString += splitString[i].slice(1);
+      newString += currentWord[0].toUpperCase();
+      newString += currentWord.slice(1);
     }
   }
+  console.log(newString)
   return newString;
 }
