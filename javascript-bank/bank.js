8f71676c5e46f8f9f8400f7a34abd8f8b22aedb8
Ken Kieu <kenkieu93@gmail.com>
2021-09-15 13:23:17 -0700

    Completed javascript-bank challenge.

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -7,7 +7,7 @@ function Bank() {
 Bank.prototype.openAccount = function (holder, balance) {
   if (Number.isInteger(balance) && Math.sign(balance) === 1) {
     var newUser = new Account(this.nextAccountNumber, holder);
-    newUser.transactions.push(balance);
+    newUser.deposit(balance);
     this.accounts.push(newUser);
     this.nextAccountNumber++;
     var userNumber = newUser.number;
@@ -17,10 +17,31 @@ Bank.prototype.openAccount = function (holder, balance) {
   }
 };
 
-Bank.prototype.getAccount = function () {
-
+Bank.prototype.getAccount = function (number) {
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (this.accounts[i].number === number) {
+      return this.accounts[i];
+    }
+  }
+  return null;
 };
 
 Bank.prototype.getTotalAssets = function () {
-
+  var deposits = 0;
+  var withdrawals = 0;
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (this.accounts.length > 0) {
+      for (var x = 0; x < this.accounts[i].transactions.length; x++) {
+        if (this.accounts[i].transactions[x].type === 'deposit') {
+          deposits += this.accounts[i].transactions[x].amount;
+        } else {
+          withdrawals += this.accounts[i].transactions[x].amount;
+        }
+      }
+    } else {
+      return 0;
+    }
+  }
+  var totalAssets = deposits - withdrawals;
+  return totalAssets;
 };
