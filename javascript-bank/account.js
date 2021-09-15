43deccf2ea0e8ab99897ecf4f2acb027bf53e38c
Ken Kieu <kenkieu93@gmail.com>
2021-09-14 22:36:34 -0700

    In Progress

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -5,4 +5,40 @@ function Account(number, holder) {
   this.transactions = [];
 }
 
-Account.prototype.
+Account.prototype.deposit = function (amount) {
+  var newDeposit = new Transaction('deposit', amount);
+  if (Number.isInteger(amount) && Math.sign(amount) === 1) {
+    this.transactions.push(newDeposit);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+Account.prototype.withdraw = function (amount) {
+  var newWithdrawal = new Transaction('withdrawal', amount);
+  if (Number.isInteger(amount) && Math.sign(amount) === 1) {
+    this.transactions.push(newWithdrawal);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+Account.prototype.getBalance = function () {
+  if (this.transactions.length > 0) {
+    var deposits = 0;
+    var withdrawals = 0;
+    for (i = 0; i < this.transactions.length; i++) {
+      if (this.transactions[i].type === 'deposit') {
+        deposits += this.transactions[i].amount
+      } else {
+        withdrawals += this.transactions[i].amount
+      }
+    }
+    var sum = deposits - withdrawals
+    return sum;
+  } else {
+    return 0;
+  }
+}
--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,1 +1,18 @@
 /* exported Bank */
+function Bank() {
+  this.nextAccountNumber = 1
+  this.accounts = [];
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+  if (balance > 0) {
+    var newUser = new Account(this.nextAccountNumber, holder);
+    var newDeposit = new Transaction('deposit', balance)
+    this.accounts.push(newUser);
+    this.nextAccountNumber++
+    var userNumber = newUser.number
+    return userNumber;
+  } else {
+    return null;
+  }
+}
