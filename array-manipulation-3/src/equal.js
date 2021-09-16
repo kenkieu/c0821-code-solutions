e8218b13055fdde85cbdd6b15ca4bbe04644c7a0
Ken Kieu <kenkieu93@gmail.com>
2021-09-15 22:06:20 -0700

    In Progress

---
+++ array-manipulation-3/src/equal.js
@@ -0,0 +1,22 @@
+/* exported equal */
+//input 1: array
+//input 2: array
+//output: boolean
+
+//if the length of the first is not equal to the second
+//return false
+// look at each character of both first and second at the same position
+// if it is not equal
+// return false
+// return true for remaining
+function equal(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+  for (var i = 0; i < first.length; i++) {
+    if (first[i] !== second[i]) {
+      return false;
+    }
+  }
+  return true;
+}
