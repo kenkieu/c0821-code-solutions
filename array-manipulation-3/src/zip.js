8f1970e5f253208c7f283aa33d314fec3c9e27d0
Ken Kieu <kennyykieu@gmail.com>
2021-10-04 21:43:09 -0700

    Revision to reduce to one loop for the shorter array.

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -2,33 +2,27 @@
 // input 1: array
 // input 2: array
 // output: An Array of Arrays
+// find the shorter array and store it into a variable shortArr
 // create outputArr
-// check each item for first input
-// inside first input check, check if second input items
+// check each item of the shortArr
 // create subArr
-// if i is the same as x
 // push first item at that position into subArr
 // push second item at that position into subArr
 // push subArr into outputArr
 // return outputArr
 
 function zip(first, second) {
-  if(first.length < second.length){
-    var loopedArr = first;
-  } else{
-    var loopedArr = second;
+  if (first.length > second.length) {
+    var shortArr = second;
+  } else {
+    shortArr = first;
   }
   var outputArr = [];
-  for (var i = 0; i < first.length; i++) {
-    for (var x = 0; x < second.length; x++) {
-      var subArr = [];
-      if (i === x) {
-        subArr.push(first[i]);
-        subArr.push(second[i]);
-        outputArr.push(subArr);
-      }
-    }
+  for (var i = 0; i < shortArr.length; i++) {
+    var subArr = [];
+    subArr.push(first[i]);
+    subArr.push(second[i]);
+    outputArr.push(subArr);
   }
-  console.log(outputArr)
   return outputArr;
 }
