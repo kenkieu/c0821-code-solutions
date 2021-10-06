da50f10b15ae6cb9060c8d1d143ebbd720beb118
Ken Kieu <kennyykieu@gmail.com>
2021-10-05 20:13:52 -0700

    Fixed 'value of require' label

--- node-module-system/a-wrapped-module.js
+++ node-module-system/a-wrapped-module.js
@@ -2,7 +2,7 @@ console.log('type of: exports:', typeof exports);
 console.log('value of exports:', exports);
 
 console.log('type of require:', typeof require);
-console.log('value of :', require);
+console.log('value of require :', require);
 
 console.log('type of module:', typeof module);
 console.log('value of module:', module);
