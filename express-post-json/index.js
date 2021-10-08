d149cc26ada35db20a39fc38f51788685edaea40
Ken Kieu <kennyykieu@gmail.com>
2021-10-08 22:42:46 +0000

    Revision for express-post-json

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const app = express();
-app.use(express.json());
 
 let nextId = 1;
 const grades = {};
-const gradesArr = [];
+
+app.use(express.json());
 
 app.get('/api/grades', (req, res) => {
+  const gradesArr = [];
+  for (const key in grades) {
+    gradesArr.push(grades[key]);
+  }
   res.json(gradesArr);
 });
 
 app.post('/api/grades', (req, res) => {
   grades[nextId] = req.body;
   grades[nextId].id = nextId;
-  gradesArr.push(grades[nextId]);
   res.status(201).json(grades);
   nextId++;
 });
