9829aaa13d83c7e428fca66ae681b1e347c542e9
Ken Kieu <kennyykieu@gmail.com>
2021-10-08 21:47:42 +0000

    Completed express-post-json exercise.

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -1,25 +1,21 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
-const nextId = 1;
+let nextId = 1;
 const grades = {};
 const gradesArr = [];
 
 app.get('/api/grades', (req, res) => {
-  for (const key in grades) {
-    gradesArr.push(grades[key]);
-  }
   res.json(gradesArr);
 });
 
-app.use(express.json());
-
 app.post('/api/grades', (req, res) => {
-  req.json({
-    name: 'Brendan Eich',
-    course: 'JavaScript',
-    score: 100
-  });
+  grades[nextId] = req.body;
+  grades[nextId].id = nextId;
+  gradesArr.push(grades[nextId]);
+  res.status(201).json(grades);
+  nextId++;
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
