611ee6abd45f68d8864770323347516b2f10e5da
Ken Kieu <kennyykieu@gmail.com>
2021-10-09 22:59:43 +0000

    AddAdded to post route and also added the delete route.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 
 const data = require('./data');
+const fs = require('fs');
+
+console.log(data);
 
 app.use(express.json());
 
@@ -25,11 +28,46 @@ app.get('/api/notes/:id', (req, res) => {
   }
 });
 
-// POST (Add handling for 400 error, and 500 error)
+// POST
 app.post('/api/notes', (req, res) => {
-  data.notes[data.nextId] = req.body;
-  data.notes[data.nextId].id = data.nextId;
-  res.status(200).json(req.body);
+// no content in req body
+  if (!req.body.content) {
+    res.status(400).json({ error: 'content is a required field' });
+  } else if (req.body.content) {
+    data.notes[data.nextId] = req.body;
+    data.notes[data.nextId].id = data.nextId;
+    const newData = JSON.stringify(data, null, 2);
+    fs.writeFile('data.json', newData, err => {
+      if (err) throw err;
+    });
+    res.status(201).json(data.notes[data.nextId]);
+    data.nextId++;
+  }
+  // Condition???
+  else if (!data) {
+    res.status(500).json({ error: 'An unexpected error has occurred' });
+  }
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  console.log(req.params);
+  if (req.params.id < 1) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (!data.notes[req.params.id]) {
+    res.status(404).json({ error: `cannot find note with id ${req.params.id}` });
+  }
+  // Condition???
+  else if (!data) {
+    res.status(500).json({ error: 'An unexpected error has occurred' });
+  } else if (req.params.id) {
+    delete data.notes[req.params.id];
+    console.log(data);
+    const newData = JSON.stringify(data, null, 2);
+    fs.writeFile('data.json', newData, err => {
+      if (err) throw err;
+    });
+    res.status(204).json();
+  }
 });
 
 app.listen(3000, () => console.log('Creepin on port 3000!'));
