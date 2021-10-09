a4321da35f4605d15c6e249be06ab13404e1d71a
Ken Kieu <kennyykieu@gmail.com>
2021-10-09 06:43:31 +0000

    Added functionality for single note, and added success route for POST.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -3,12 +3,33 @@ const app = express();
 
 const data = require('./data');
 
+app.use(express.json());
+
+// READ
 app.get('/api/notes', (req, res) => {
   const notesArr = [];
   for (const key in data.notes) {
     notesArr.push(data.notes[key]);
   }
   res.status(200).json(notesArr);
+});
+
+// READ - Single Note (Account for strings?)
+app.get('/api/notes/:id', (req, res) => {
+  if (typeof req.params.id < 1) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (!data.notes[req.params.id]) {
+    res.status(404).json({ error: `cannot find note with id ${req.params.id}` });
+  } else {
+    res.status(200).json(data.notes[req.params.id]);
+  }
+});
+
+// POST (Add handling for 400 error, and 500 error)
+app.post('/api/notes', (req, res) => {
+  data.notes[data.nextId] = req.body;
+  data.notes[data.nextId].id = data.nextId;
+  res.status(200).json(req.body);
 });
 
 app.listen(3000, () => console.log('Creepin on port 3000!'));
