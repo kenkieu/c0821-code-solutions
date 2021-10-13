78fd9fab7b31c91715f194b4531f64dabb16ae33
Ken Kieu <kennyykieu@gmail.com>
2021-10-13 21:39:39 +0000

    Chained then and catchch methods.

--- es6-promises/index.js
+++ es6-promises/index.js
@@ -1,6 +1,4 @@
 const takeAChance = require('./take-a-chance');
 
 const nameLottery = takeAChance('Ken');
-
-nameLottery.then(fulfillment => console.log(fulfillment));
-nameLottery.catch(error => console.log(error.message));
+nameLottery.then(fulfillment => console.log(fulfillment)).catch(error => console.log(error.message));
