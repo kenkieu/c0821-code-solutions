067c0f3c580689fdbe67c4e1bb4bfbf5e458a76a
Ken Kieu <kennyykieu@gmail.com>
2021-10-21 22:17:33 +0000

    Initialized project with dependencies, and created necessary files.

---
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class AppDrawer extends React.Component {
+  // constructor(props){
+
+  // }
+  render() {
+    return (
+      <>
+        <i className="fas fa-bars"></i>
+      </>
+    );
+  }
+}
+
+export default AppDrawer;
