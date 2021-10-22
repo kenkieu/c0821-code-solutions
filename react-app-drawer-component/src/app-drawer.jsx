f0c79771f8e1c3930b7e8a94f19468a64b6f2de1
Ken Kieu <kennyykieu@gmail.com>
2021-10-21 22:43:21 -0700

    Toggle menu finished, transition implementation in process.

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -6,17 +6,23 @@ class AppDrawer extends React.Component {
   constructor(props) {
     super(props);
     this.state = { menuOpen: 'false' };
-    this.createMenuItems = this.createMenuItems.bind(this);
     this.toggleMenu = this.toggleMenu.bind(this);
   }
 
   render() {
     if (!this.state.menuOpen) {
       return (
-        <div className="menu-container">
-          <h2 className="menu-header">Menu</h2>
-          {this.createMenuItems()}
+        <>
+        <div className="container menu-bg">
+          <div className="row">
+            <div className="menu">
+              <h2 className="menu-title">Menu</h2>
+              {this.createMenuItems()}
+            </div>
+              <div onClick={this.toggleMenu} className="site"></div>
+          </div>
         </div>
+        </>
       );
     } else {
       return <i onClick={this.toggleMenu} className="fas fa-bars"></i>;
@@ -25,7 +31,7 @@ class AppDrawer extends React.Component {
 
   createMenuItems() {
     const list = menuItems.map(item =>
-    <li onClick={this.toggleMenu} key={item}>{item}</li>
+      <li onClick={this.toggleMenu} key={item}>{item}</li>
     );
     return <ul>{list}</ul>;
   }
