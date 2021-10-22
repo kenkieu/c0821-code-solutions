a5d09d248670461c8332755d0941a27191acc13b
Ken Kieu <kennyykieu@gmail.com>
2021-10-22 20:23:30 +0000

    Completed react-app-drawer-component challenge.

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -1,43 +1,62 @@
 import React from 'react';
 
-const menuItems = ['About', 'Get Started', 'Sign In'];
+const menuItems = ['Lost Ark', 'Minecraft', 'New World', 'Valheim', 'Valorant'];
 
 class AppDrawer extends React.Component {
   constructor(props) {
     super(props);
     this.state = { menuOpen: 'false' };
-    this.toggleMenu = this.toggleMenu.bind(this);
+    this.openMenu = this.openMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
   }
 
   render() {
-    if (!this.state.menuOpen) {
+    if (this.state.menuOpen === 'true') {
       return (
         <>
-        <div className="container menu-bg">
-          <div className="row">
-            <div className="menu">
-              <h2 className="menu-title">Menu</h2>
-              {this.createMenuItems()}
+          <i onClick={this.openMenu} className="fas fa-bars hamburger-icon"></i>
+          <div className="container menu-bg">
+            <div className="row">
+              <div className="menu menu-open">
+                <h2 className="menu-title">Select A Game</h2>
+                {this.createMenuItems()}
+              </div>
+              <div onClick={this.closeMenu} className="site"></div>
             </div>
-              <div onClick={this.toggleMenu} className="site"></div>
           </div>
-        </div>
         </>
       );
     } else {
-      return <i onClick={this.toggleMenu} className="fas fa-bars"></i>;
+      return (
+          <>
+            <i onClick={this.openMenu} className="fas fa-bars hamburger-icon"></i>
+          <div className="container">
+              <div className="row">
+              <div className="menu">
+                <h2 className="menu-title">Menu</h2>
+                {this.createMenuItems()}
+              </div>
+              <div onClick={this.closeMenu} className="site"></div>
+              </div>
+            </div>
+          </>
+      );
     }
   }
 
   createMenuItems() {
     const list = menuItems.map(item =>
-      <li onClick={this.toggleMenu} key={item}>{item}</li>
+      <li onClick={this.closeMenu} key={item}>{item}</li>
     );
     return <ul>{list}</ul>;
   }
 
-  toggleMenu() {
-    this.setState({ menuOpen: !this.state.menuOpen });
+  openMenu() {
+    this.setState({ menuOpen: 'true' });
+  }
+
+  closeMenu() {
+    this.setState({ menuOpen: 'false' });
   }
 }
 
