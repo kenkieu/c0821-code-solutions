cccb0580d96ccad4513db0edf301e300f61718aa
Ken Kieu <kennyykieu@gmail.com>
2021-10-22 01:08:52 +0000

    Added menu and open/close on click of link element and hamburger menu.

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 
-class AppDrawer extends React.Component {
-  // constructor(props){
+const menuItems = ['About', 'Get Started', 'Sign In'];
 
-  // }
+class AppDrawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { menuOpen: 'false' };
+    this.createMenuItems = this.createMenuItems.bind(this);
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
   render() {
-    return (
-      <>
-        <i className="fas fa-bars"></i>
-      </>
+    if (!this.state.menuOpen) {
+      return (
+        <div className="menu-container">
+          <h2 className="menu-header">Menu</h2>
+          {this.createMenuItems()}
+        </div>
+      );
+    } else {
+      return <i onClick={this.toggleMenu} className="fas fa-bars"></i>;
+    }
+  }
+
+  createMenuItems() {
+    const list = menuItems.map(item =>
+    <li onClick={this.toggleMenu} key={item}>{item}</li>
     );
+    return <ul>{list}</ul>;
+  }
+
+  toggleMenu() {
+    this.setState({ menuOpen: !this.state.menuOpen });
   }
 }
 
