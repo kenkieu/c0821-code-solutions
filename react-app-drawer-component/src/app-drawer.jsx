d19b8b194520fd224d153b67d0b8d2e7d42f4a18
Ken Kieu <kennyykieu@gmail.com>
2021-10-25 20:50:10 +0000

    Revision for react-app-drawer-component challenge.

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -11,13 +11,19 @@ class AppDrawer extends React.Component {
   }
 
   render() {
-    if (this.state.menuOpen === 'true') {
-      return (
+    const menuClassName = this.state.menuOpen === 'true'
+      ? 'menu menu-open'
+      : 'menu';
+
+    const containerClassName = this.state.menuOpen === 'true'
+      ? 'container menu-bg'
+      : 'container';
+    return (
         <>
           <i onClick={this.openMenu} className="fas fa-bars hamburger-icon"></i>
-          <div className="container menu-bg">
+          <div className={containerClassName}>
             <div className="row">
-              <div className="menu menu-open">
+              <div className={menuClassName}>
                 <h2 className="menu-title">Select A Game</h2>
                 {this.createMenuItems()}
               </div>
@@ -25,23 +31,7 @@ class AppDrawer extends React.Component {
             </div>
           </div>
         </>
-      );
-    } else {
-      return (
-          <>
-            <i onClick={this.openMenu} className="fas fa-bars hamburger-icon"></i>
-          <div className="container">
-              <div className="row">
-              <div className="menu">
-                <h2 className="menu-title">Menu</h2>
-                {this.createMenuItems()}
-              </div>
-              <div onClick={this.closeMenu} className="site"></div>
-              </div>
-            </div>
-          </>
-      );
-    }
+    );
   }
 
   createMenuItems() {
