813dddbe110b075477c412104d9fa5033e03f032
Ken Kieu <kennyykieu@gmail.com>
2021-10-25 23:23:49 +0000

    Initial project setup.

---
+++ react-carousel-component/src/carousel.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <>
+      <div className="container border-black">
+        <div className="row">
+        </div>
+      </div>
+    </>;
+  }
+}
+
+export default Carousel;
