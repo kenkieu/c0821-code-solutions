3fb7d9e9e9d350ad7f014b284a23765c735f66fa
Ken Kieu <kennyykieu@gmail.com>
2021-10-27 00:52:44 +0000

    Revision for api-registration exercise.

--- api-registration/server/index.js
+++ api-registration/server/index.js
@@ -30,7 +30,7 @@ app.post('/api/auth/sign-up', (req, res, next) => {
       const sql = `
        insert into "users" ("username", "hashedPassword")
        values ($1, $2)
-       returning *
+       returning "username"
     `;
       const params = [username, hashedPassword];
       return db.query(sql, params);
