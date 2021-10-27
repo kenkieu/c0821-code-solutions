25c0a00826f4fe47e62b79fa6a44528da35f9e82
Ken Kieu <kennyykieu@gmail.com>
2021-10-27 04:03:39 +0000

    Added prev and next slide functionality, interval in progress.

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -11,25 +11,56 @@ const carousel = [
 class Carousel extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { imageNumber: 0 };
+    this.state = {
+      imageNumber: 0
+    };
+    this.prevImage = this.prevImage.bind(this);
+    this.nextImage = this.nextImage.bind(this);
+    this.interval = this.interval.bind(this);
   }
 
   render() {
     return <>
       <div className="container border-black">
         <div className="row">
-          <div className="column-twelve-and-half">
-
+          <div className="column-twelve-and-half justify-center align-center">
+            <i onClick={this.prevImage} className="fas fa-chevron-left chevron"></i>
           </div>
-          <div className="column-seventy-five justify-center align-center">
-            <img className="" src={`${carousel[this.state.imageNumber].src}`} alt={`${carousel[this.state.imageNumber].name}`} />
+          <div className="column-seventy-five justify-center align-center flex-wrap">
+            <img className="" data-img-id={this.state.imageNumber} src={`${carousel[this.state.imageNumber].src}`} alt={`${carousel[this.state.imageNumber].name}`} />
+            <div className="column-full dots justify-center" key={carousel.name}>
+            </div>
           </div>
-          <div className="column-twelve-and-half">
-
+          <div className="column-twelve-and-half justify-center align-center">
+            <i onClick={this.nextImage} className="fas fa-chevron-right chevron"></i>
           </div>
         </div>
       </div>
     </>;
+  }
+
+  nextImage() {
+    if (this.state.imageNumber < carousel.length - 1) {
+      this.setState({ imageNumber: this.state.imageNumber + 1 });
+      console.log(this.state.imageNumber);
+    } else {
+      this.setState({ imageNumber: 0 });
+    }
+  }
+
+  prevImage() {
+    if (this.state.imageNumber > 0) {
+      this.setState({ imageNumber: this.state.imageNumber - 1 });
+    } else {
+      console.log(carousel.length);
+      this.setState({ imageNumber: carousel.length - 1 });
+    }
+  }
+
+  interval() {
+    this.timerId = setInterval(() => {
+      this.setState({ imageNumber: this.state.imageNumber + 1 });
+    }, 1000);
   }
 }
 
