2e6603b20864703f37b213f83f1103597f436fd5
Ken Kieu <kennyykieu@gmail.com>
2022-02-23 14:44:31 -0800

    Review view swapping

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -1,20 +1,20 @@
 function handleClick(event) {
   if (event.target.matches('.tab')) {
-    for (var i = 0; i < $tab.length; i++) {
-      if ($tab[i] === event.target) {
-        $tab[i].className = 'tab active';
-        var attr = $tab[i].getAttribute('data-view');
+    let tabView = '';
+    for (const tab of $tab) {
+      if (tab === event.target) {
+        tab.className = 'tab active';
+        tabView = tab.getAttribute('data-view');
       } else {
-        $tab[i].className = 'tab';
+        tab.className = 'tab';
       }
     }
 
-    for (var x = 0; x < $view.length; x++) {
-      if ($view[x].dataset.view === attr) {
-        $view[x].className = 'view';
-      } else {
-        $view[x].className = 'view hidden';
-      }
+    for (const view of $view) {
+      const dataView = view.dataset.view;
+      dataView === tabView
+        ? view.className = 'view'
+        : view.className = 'view hidden';
     }
   }
 }
